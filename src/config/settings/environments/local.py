cc927feeb37dfd2de99cde998f560db38d090fd9
Shadaab <karimshadaab510@gmail.com>
2025-07-19 19:26:23 +0530

    chore: added debug_toolbar and pre-commit

--- src/config/settings/environments/local.py
+++ src/config/settings/environments/local.py
@@ -2,21 +2,29 @@
 # Local Environment Settings
 # ==============================================================================
 
-# Import base development settings
-from config.settings.environments.development import *
-
 # Import installed apps from the app configuration
 from config.settings.components.app import INSTALLED_APPS
+from config.settings.components.middleware import MIDDLEWARE
+
+# Import base development settings
+from config.settings.environments.development import *  # noqa
 
 # ------------------------------------------------------------------------------
 # Local Development Specific Apps
 # ------------------------------------------------------------------------------
 _LOCAL_DEV_APPS = [
     "django_extensions",  # Useful extensions for Django development
+    "debug_toolbar",  # Django Debug Toolbar for debugging
 ]
 
 # Add local development apps to the installed apps list
-INSTALLED_APPS.append(*_LOCAL_DEV_APPS)
+INSTALLED_APPS += _LOCAL_DEV_APPS
+
+# ------------------------------------------------------------------------------
+# Middleware Configurations
+# ------------------------------------------------------------------------------
+
+MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
 
 # ------------------------------------------------------------------------------
 # Django Shell Plus Configuration
@@ -38,6 +46,16 @@ SHELL_PLUS_SQLPARSE_FORMAT_KWARGS = dict(
     reindent_aligned=True,  # Align SQL reindentation
     truncate_strings=500,  # Truncate long strings in SQL output
 )
+
+# ------------------------------------------------------------------------------
+# Internal IPs for Debug Toolbar
+# ------------------------------------------------------------------------------
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 
 # ==============================================================================
 # End of Local Environment Settings
--- src/scripts/create_fake_superuser.py
+++ src/scripts/create_fake_superuser.py
@@ -1,18 +1,19 @@
-from faker import Faker
 from django.contrib.auth import get_user_model
+from faker import Faker
+
 
 def run():
     fake = Faker()
-    
+
     User = get_user_model()
-    
+
     username = fake.user_name()
     email = fake.email()
     password = fake.password()
-    
+
     if not User.objects.filter(username=username).exists():
         User.objects.create_superuser(username=username, email=email, password=password)
-        
+
         print(f"Created superuser : {username} {email} | Password : {password}")
     else:
         print("User already exists.")
